65715b6f8f5000f24d37c8857658c225405fb9a8
Tuozzo <lucas.javier.tuozzo@accenture.com>
2019-08-27 16:14:14 -0300

    CV terminado

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//Esto lo agregué para que queden más lindos los anchor links, lo saqué de acá: https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
